72ada2d7ff229d556de54a5c1dc870d327269901
Junior Jimenez Encarnacion <jimenezfrankjunior@gmail.com>
2025-04-02 11:13:40 +0200

    99 bottles of beer

---
+++ Loops/99_bottles.py
@@ -0,0 +1,5 @@
+for i in range(99):
+    print(f'{i} bottles of beer on the wall')
+    print(f'{i} bottles of beer')
+    print('Take one down, pass it around')
+    print(f'{i} bottles of beer on the wall')
