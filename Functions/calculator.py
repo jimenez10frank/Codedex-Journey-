16e0de794330bff3ee4bd94cf626f93dd11385f5
Junior Jimenez Encarnacion <jimenezfrankjunior@gmail.com>
2025-04-06 16:36:52 +0200

    more functions exercices

---
+++ Functions/calculator.py
@@ -0,0 +1,21 @@
+def add(a, b):
+    answer = a + b
+    return answer
+def substract(a, b):
+    answer = a - b
+    return answer
+def multiply(a, b):
+    answer = a * b
+    return answer
+def divide(a, b):
+    answer = a / b
+    return answer
+def exp(a, b):
+    answer = a % b
+    return answer
+
+print(add(2,2))
+print(substract(6,8))
+print(multiply(2,52))
+print(divide(9,5))
+print(exp(9,14))
---
+++ Functions/rocket.py
@@ -0,0 +1,6 @@
+def distance_to_miles(distance):
+    return distance / 1.609
+
+answer = distance_to_miles(1000)
+print(answer)
+    
