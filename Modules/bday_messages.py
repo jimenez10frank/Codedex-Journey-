037d24f46f974e349a8f16578224d9680af3ea64
Junior Jimenez Encarnacion <jimenezfrankjunior@gmail.com>
2025-04-09 00:03:54 +0200

    more modules exercices

---
+++ Modules/bday_messages.py
@@ -0,0 +1,10 @@
+import random
+
+bday_messages = [
+'Hope you have a very Happy Birthday! 🎈',
+'Its your special day get out there and celebrate! 🎉',
+'You were born and the world got better – everybody wins! 🥳',
+'Have lots of fun on your special day! 🎂',
+'Another year of you going around the sun! 🌞']
+
+random_message = random.choice(bday_messages)
