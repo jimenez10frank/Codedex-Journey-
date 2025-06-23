b315b25e1978a57f695ed7a60665f6b0d03150f0
Junior Jimenez Encarnacion <jimenezfrankjunior@gmail.com>
2025-06-23 13:53:15 +0200

    javascript begin

---
+++ JavascriptBeginner/console/comment.js
@@ -0,0 +1,3 @@
+// Coca-Cola formula.
+// Kentucky Fried Chicken (KFC)'s 11 herbs and spices.
+// The sauce used in a Big Mac at McDonald's.
---
+++ JavascriptBeginner/variables/constlet.js
@@ -0,0 +1,9 @@
+let firstName = "Junior";
+let lastName = "Jimenez Encarnacion"
+
+const age = 19;
+const country = "The Netherlands";
+
+firstName = "Frank Junior";
+
+console.log(firstName + lastName);
