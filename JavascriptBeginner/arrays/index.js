8376634d3a6be1bbe7dbc5dd30946babb96351a1
Junior Jimenez Encarnacion <jimenezfrankjunior@gmail.com>
2025-07-12 22:17:30 +0200

    Add beginner array examples for JavaScript
    Introduced four new files demonstrating basic array operations: listing destinations, modifying a grocery list, iterating over music notes, and generating a multiplication table. These examples are intended for JavaScript beginners to practice array manipulation and iteration.

---
+++ JavascriptBeginner/arrays/index.js
@@ -0,0 +1,7 @@
+let groceryList = ["🥛 Milk", "🥑 Avocado", "🥚 Eggs", "🍞 Bread"];
+
+groceryList[2] = "butter";
+
+groceryList[4] = "Laundry Soap";
+
+console.log(groceryList);
---
+++ JavascriptBeginner/arrays/music.js
@@ -0,0 +1,7 @@
+const musicNotes = ["Do", "Re", "Mi", "Fa", "So", "La", "Ti"];
+let counter = 0;
+
+while (counter < musicNotes.length) {
+  console.log(musicNotes[counter]);
+  counter++;
+}
