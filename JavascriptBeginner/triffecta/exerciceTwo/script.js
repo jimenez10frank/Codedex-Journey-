3e738cc318f0f8380eb1a410d190434679088105
Junior Jimenez Encarnacion <jimenezfrankjunior@gmail.com>
2025-07-18 13:57:18 +0200

    Add four beginner JavaScript exercises
    Introduces four exercises: a traffic light clicker, a mood ring color generator, a quote generator with color changes, and a simple image/text swap. Each exercise includes its own HTML, CSS, and JavaScript files for interactive learning.

---
+++ JavascriptBeginner/triffecta/exerciceTwo/script.js
@@ -0,0 +1,4 @@
+const drakeTwo = document.getElementById("drake-pic-2");
+const heading = document.getElementById("heading-2");
+drakeTwo.src = "https://i.imgur.com/RGbh6zY.png";
+heading.innerText = "Learn by doing";
